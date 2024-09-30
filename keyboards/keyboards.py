38848a001921085a52dc91b19588e946e4dc34db
Lex <166012830+xelvhk@users.noreply.github.com>
2024-09-30 10:41:28 +0000

    added stat buttons to keyboard

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -5,6 +5,8 @@ from lexicon.lexicon_ru import LEXICON_RU
 # Создаем кнопки клавиатуры
 yatut_button_1 = KeyboardButton(text=LEXICON_RU['answers']['tut'])
 yatut_button_2 = KeyboardButton(text=LEXICON_RU['answers']['netut'])
+yatut_button_3 = KeyboardButton(text=LEXICON_RU['week'])
+yatut_button_4 = KeyboardButton(text=LEXICON_RU['month'])
 
 # Создаем клавиатуру функций
 yatut_kb = ReplyKeyboardMarkup(
